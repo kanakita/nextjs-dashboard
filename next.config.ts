a4edb2495758febc4204f8a68d3af94b1175643b
kana.kita <kitagawa@ham.works>
2025-09-11 16:21:53 +0900

    検索とページネーションまで

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // チュートリアルにはあるが、エラーが出るのでコメントアウト
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
 };
 
 export default nextConfig;
